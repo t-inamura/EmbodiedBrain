605a731a08e27a3e2bbb9371fa952385e2b2e75e
ubuntu <sigverse@ubuntu.(none)>
2014-12-03 10:20:24 +0900

    Rehabilitation program of phantom limb pain with KinectV2]

--- MirrorTherapy/Server/Controller/make.sh
+++ MirrorTherapy/Server/Controller/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # For Right Hand Grasping
-g++ Linkage_Grasp.cpp -shared  -fPIC -o Linkage_Grasp.so                        -DUSE_ODE -DCONTROLLER -DdDOUBLE                  -D_VERBOSE             -I /home/sigverse/sigverse-2.2.0/include 
+g++ Linkage_Grasp.cpp -shared  -fPIC -o Linkage_Grasp.so   -DUSE_ODE -DCONTROLLER -DdDOUBLE  -I /home/sigverse/sigverse-2.2.0/include 
 # For Grasped Object
-g++ Linkage_Object.cpp -shared -fPIC -o Linkage_Obon.so                         -DUSE_ODE -DCONTROLLER -DdDOUBLE                  -D_VERBOSE             -I /home/sigverse/sigverse-2.2.0/include
+g++ Linkage_Object.cpp -shared -fPIC -o Linkage_Obon.so    -DUSE_ODE -DCONTROLLER -DdDOUBLE  -I /home/sigverse/sigverse-2.2.0/include
