5b1b8d539b91b84fdeba6ae92e4a40c65ce4455b
tome-nozaki <tome-nozaki@nii.ac.jp>
2015-03-18 19:31:14 +0900

    Update source code.

---
+++ MirrorTherapy_v2/Plugin/OculusRiftDK1_vs2010/OculusRiftDK1_vs2010/OculuRiftDK1Device.cpp
@@ -0,0 +1,11 @@
+#include "OculuRiftDK1Device.h"
+
+
+OculuRiftDK1Device::OculuRiftDK1Device(void)
+{
+}
+
+
+OculuRiftDK1Device::~OculuRiftDK1Device(void)
+{
+}
---
+++ MirrorTherapy_v2/Plugin/OculusRiftDK1_vs2010/OculusRiftDK1_vs2010/OculuRiftDK1Device.h
@@ -0,0 +1,8 @@
+#pragma once
+class OculuRiftDK1Device
+{
+public:
+	OculuRiftDK1Device(void);
+	~OculuRiftDK1Device(void);
+};
+
