c34cf223ae940e0417aee44a37b13eacab137343
tome-yamada <bombe@cpost.plala.or.jp>
2015-06-30 17:15:56 +0900

    Use sensorDataMode

--- MirrorTherapy_v2/Controller/ManNiiAvatarControllerByKinectV2/ManNiiAvatarControllerByKinectV2.h
+++ MirrorTherapy_v2/Controller/ManNiiAvatarControllerByKinectV2/ManNiiAvatarControllerByKinectV2.h
@@ -16,20 +16,30 @@
 #include "../../Common/KinectV2SensorData.h"
 
 
-#define PARAM_FILE_NAME_KINECTV2_INI  "KinectV2.ini"
-
-
 class ManNiiAvatarControllerByKinectV2 : public AvatarController
 {
+public:
 private:
 	///@brief Parameter file name.
-	std::string parameterFileName;
+	static const std::string parameterFileName;
+
+	//Parameter file information
+	static const std::string paramFileKeyKinectV2SensorDataMode;
+	static const KinectV2SensorData::SensorDataMode paramFileValKinectV2SensorDataModeDefault;
+
 
 	//ManNiiAvatarPosture posture;
 	BaseService *kinectV2Service;
 	std::string kinectV2ServiceName;
 	std::string kinectV2DeviceType;
 	std::string kinectV2DeviceUniqueID;
+
+	std::string sensorDataModeStr;
+
+	//test output file tome-yamada start
+	void testPrint();
+	std::vector<std::string> timeInfoList;
+	//test output file tome-yamada end
 
 	void readIniFile();
 
@@ -45,16 +55,10 @@ public:
 
 	///@brief Convert euler angle to avatar posture structure.
 	void convertKinectV2JointOrientations2ManNiiPosture(KinectV2SensorData::KinectV2JointOrientation *kinectV2Joints, ManNiiPosture &manNiiPosture);
-	//void convertKinectV2JointOrientations2ManNiiPosture(std::vector<KinectV2JointOrientation> &kinectV2Joints, ManNiiPosture &manNiiPosture);
 
 	void setJointQuaternion(SimObj *obj, ManNiiPosture::ManNiiJoint &jq);
 
 	void setJointQuaternionsForKinect(SimObj *obj, ManNiiPosture &manNiiPosture);
 };
-
-extern "C" Controller * createController()
-{
-	return new ManNiiAvatarControllerByKinectV2;
-}
 
 #endif //__MAN_NII_AVATAR_CONTROLLER_BY_KINECT_V2_H__
