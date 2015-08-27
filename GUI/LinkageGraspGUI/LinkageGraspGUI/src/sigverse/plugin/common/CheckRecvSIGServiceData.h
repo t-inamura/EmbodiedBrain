a276a996434745480a7c409b03b5918069fab2ef
tome-yamada <yamada4tec@gmail.com>
2015-08-27 18:12:01 +0900

    Make LinkageGraspGUI. This is still in production.

---
+++ GUI/LinkageGraspGUI/LinkageGraspGUI/src/sigverse/plugin/common/CheckRecvSIGServiceData.h
@@ -0,0 +1,17 @@
+﻿#ifndef SIGVERSE_CHECK_RECV_SIGSERVICE_DATA_H
+#define SIGVERSE_CHECK_RECV_SIGSERVICE_DATA_H
+
+#include <SIGService/SIGService.h>
+
+/*
+ * SIGServiceデータ受信チェッククラス
+ */
+class CheckRecvSIGServiceData
+{
+private:
+
+public:
+	void run(sigverse::SIGService *m_srv);
+};
+
+#endif // SIGVERSE_CHECK_RECV_SIGSERVICE_DATA_H
